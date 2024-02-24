c0ac86bb064fcca3e64cda361007419540be2292
Yilin Yang <yyl_20050115@hotmail.com>
2024-02-25 00:29:39 +0800

    fx

--- CSMatrix/CharUtils.cs
+++ CSMatrix/CharUtils.cs
@@ -5,16 +5,17 @@ public static class CharUtils
     private static readonly Random random = new();
 
     public static char GetRandomChar() 
-        => random.Next(100) switch
-    {
-        < 25 => (char)random.Next(33, 127),
-        _ => random.Next(100) switch
-        {
-            < 33 => (char)random.Next(48, 58),
-            < 66 => (char)random.Next(65, 91),
-            _ => (char)random.Next(97, 123)
+        => (char)(Next(100) switch {
+        < 25 => Next(33, 127),
+        _ => Next(100) switch {
+            < 33 => Next(48, 58),
+            < 66 => Next(65, 91),
+            _ => Next(97, 123)
         }
-    };
+    });
+
+    public static int Next(int limit)
+        => random.Next(limit);
 
     public static int Next(int minValue, int maxValue) 
         => random.Next(minValue, maxValue);
